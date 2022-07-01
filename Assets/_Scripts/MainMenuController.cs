5ae4b37e8b5fa4e13115966ee590a5f353038457
Michael Bironneau <mbironneau@gmail.com>
2022-07-01 15:41:16 +0100

    Generate lighting data and add menu controls

--- Assets/_Scripts/MainMenuController.cs
+++ Assets/_Scripts/MainMenuController.cs
@@ -4,15 +4,30 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] string tutorialScene;
+    [SerializeField] string mainMenuScene;
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("Loading main menu");
+        SceneManager.LoadScene(mainMenuScene, LoadSceneMode.Single);
+    }
     public void LoadTutorial()
     {
         Debug.Log("Loading tutorial");
-        SceneManager.LoadScene(tutorialScene);
+        SceneManager.LoadScene(tutorialScene, LoadSceneMode.Single);
     }
 
     public void Quit()
     {
         Debug.Log("Quitting");
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            LoadMainMenu();
+        }
     }
 }
